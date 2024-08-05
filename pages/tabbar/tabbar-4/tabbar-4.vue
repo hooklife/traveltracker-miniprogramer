a60806b4144cd309661bd63d34fdf91572608a5a
RISE <you@example.com>
2024-08-05 20:03:28 +0800

    init file

---
+++ pages/tabbar/tabbar-4/tabbar-4.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="demo">
+		<web-view src="@/static/hybrid/html/map.html"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+		margin-top: 200upx;
+	}
+</style>
